cee424bd5e4d43a418df84fe553be41deff30e0e
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-09-09 13:44:53 +0200

    feat: Loan view model

---
+++ Book_Library/ViewModels/LoanViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Book_Library.Models;
+using System.Collections.Generic;
+
+namespace Book_Library.Controllers
+{
+    public class LoanViewModel
+    {
+        public IEnumerable<Book> BurrowedBook { get; set; }
+        public IEnumerable<Loan> Loan { get; set; }
+    }
+}
