822fd7f88305802e3579eb7b71c876e43fa4f7ee
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-09-09 13:44:05 +0200

    feat: Burrower controller

---
+++ Book_Library/Controllers/BurrowerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Book_Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Library.Controllers
+{
+    public class BurrowerController : Controller
+    {
+        private IBurrower _burrower;
+
+        public BurrowerController(IBurrower burrower)
+        {
+            _burrower = burrower;
+        }
+
+        public IActionResult Loans(int id)
+        {
+            IEnumerable<LoanViewModel> BurrowedBooks;
+            BurrowedBooks = (IEnumerable<LoanViewModel>)
+                _burrower.ReadAllBurrowersLoans(id);
+
+            if (BurrowedBooks == null)
+            {
+                return NotFound();
+            }
+            return View(BurrowedBooks);
+        }
+    }
+}
